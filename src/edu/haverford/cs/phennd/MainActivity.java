81fe6eee211dfe2c1c78d3e4c5fe3ee03228d38b
Dan G <dangiovannelli@gmail.com>
2013-04-01 18:12:04 -0400

    Buttons replaced with ActionBar

--- src/edu/haverford/cs/phennd/MainActivity.java
+++ src/edu/haverford/cs/phennd/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.haverford.cs.phennd;
 
 import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
@@ -11,57 +14,82 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
 
-public class MainActivity extends Activity {
+public class MainActivity<T> extends Activity {
 	
 	DataManager dataManager = new DataManager();
 
+	class TabListener implements ActionBar.TabListener {
+    	private Activity activity;
+    	private int LaunchCode;
+    	
+    	public TabListener(Activity activity, int launchCode) {
+    		this.activity = activity;
+    		this.LaunchCode = launchCode;
+    	}
+
+		@Override
+		public void onTabReselected(Tab tab, FragmentTransaction ft) {
+			// Do nothing for now
+		}
+
+		@Override
+		public void onTabSelected(Tab tab, FragmentTransaction ft) {
+    		if (this.LaunchCode == 0) {
+    			if (this.activity.getClass() != MainActivity.class) {
+    				startActivity(new Intent(this.activity, MainActivity.class));				
+    			}
+    		} else if (this.LaunchCode == 1) {
+    			if (this.activity.getClass() != FavoritesView.class) {
+    				startActivity(new Intent(this.activity, FavoritesView.class));				
+    			}
+    		} else if (this.LaunchCode == 2) {
+    			if (this.activity.getClass() != TagView.class) {
+    				startActivity(new Intent(this.activity, TagView.class));				
+    			}
+    		} else if (this.LaunchCode == 3) {
+    			if (this.activity.getClass() != SettingsView.class) {
+    				startActivity(new Intent(this.activity, SettingsView.class));				
+    			}
+    		}
+		}
+
+		@Override
+		public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+			// Do nothing for now
+		}
+	}
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        ActionBar actionBar = getActionBar();
+        actionBar.setTitle("Article Categories");
+        actionBar.setSubtitle("PHENND Update");
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
+        Tab tabCategory = actionBar.newTab();
+        tabCategory.setText("Categories").setIcon(R.drawable.ic_launcher).setTabListener(new TabListener(this,0));
+        Tab tabFavorites = actionBar.newTab();
+        tabFavorites.setText("Favorites").setIcon(R.drawable.ic_launcher).setTabListener(new TabListener(this, 1));
+        Tab tabTags = actionBar.newTab();
+        tabTags.setText("Tags").setIcon(R.drawable.ic_launcher).setTabListener(new TabListener(this, 2));
+        Tab tabSettings = actionBar.newTab();
+        tabSettings.setText("Settings").setIcon(R.drawable.ic_launcher).setTabListener(new TabListener(this, 3));
+
+        actionBar.addTab(tabCategory, true);
+        actionBar.addTab(tabFavorites, 1, false);
+        actionBar.addTab(tabTags, 2, false);
+        actionBar.addTab(tabSettings, 3, false);
+        actionBar.selectTab(tabCategory);
+        
         String[] categories = {"Grant Opportunities", "Job Opportunities/AmeriCorps Opportunities", "K-16 Partnerships", "For Students","Miscellaneous","National Conferences & Calls for Proposal","New Resources","Other Local Events and workshops","Partnerships Classifieds","PHENND Events/Activities"};
 		ListView listView = (ListView) findViewById(R.id.listView1);
 		
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, categories);
 		listView.setAdapter(adapter); 
-
-        final Button tagViewButton = (Button) findViewById(R.id.buttonTags1);
-        tagViewButton.setOnClickListener(
-                new View.OnClickListener()
-                {
-                        @Override
-                        public void onClick(View v)
-                        {
-                            Intent intent = new Intent(v.getContext(), TagView.class);
-                            startActivityForResult(intent, 0);
-                        }
-                });
-
-        final Button favoritesViewButton = (Button) findViewById(R.id.buttonFavorites1);
-        favoritesViewButton.setOnClickListener(
-                new View.OnClickListener()
-                {
-                        @Override
-                        public void onClick(View v)
-                        {
-                            Intent intent = new Intent(v.getContext(), FavoritesView.class);
-                            startActivityForResult(intent, 0);
-                        }
-                });
-        
-        final Button settingsViewButton = (Button) findViewById(R.id.buttonSettings1);
-        settingsViewButton.setOnClickListener(
-                new View.OnClickListener()
-                {
-                        @Override
-                        public void onClick(View v)
-                        {
-                            Intent intent = new Intent(v.getContext(), SettingsView.class);
-                            startActivityForResult(intent, 0);
-                        }
-                });
-        
+       
        
        // I think that this is why sometimes articles don't load - I might be crazy (KV)
        Thread t = new Thread() { 
@@ -91,11 +119,5 @@ public class MainActivity extends Activity {
         
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
+
 }
--- src/edu/haverford/cs/phennd/TagView.java
+++ src/edu/haverford/cs/phennd/TagView.java
@@ -3,8 +3,13 @@ package edu.haverford.cs.phennd;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.haverford.cs.phennd.MainActivity.TabListener;
+
 import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.app.ActionBar.Tab;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
@@ -18,47 +23,75 @@ public class TagView extends Activity {
 
     List<String> flaggedTags;
 
+	class TabListener implements ActionBar.TabListener {
+    	private Activity activity;
+    	private int LaunchCode;
+    	
+    	public TabListener(Activity activity, int launchCode) {
+    		this.activity = activity;
+    		this.LaunchCode = launchCode;
+    	}
+
+		@Override
+		public void onTabReselected(Tab tab, FragmentTransaction ft) {
+			// Do nothing for now
+		}
+
+		@Override
+		public void onTabSelected(Tab tab, FragmentTransaction ft) {
+    		if (this.LaunchCode == 0) {
+    			if (this.activity.getClass() != MainActivity.class) {
+    				startActivity(new Intent(this.activity, MainActivity.class));				
+    			}
+    		} else if (this.LaunchCode == 1) {
+    			if (this.activity.getClass() != FavoritesView.class) {
+    				startActivity(new Intent(this.activity, FavoritesView.class));				
+    			}
+    		} else if (this.LaunchCode == 2) {
+    			if (this.activity.getClass() != TagView.class) {
+    				startActivity(new Intent(this.activity, TagView.class));				
+    			}
+    		} else if (this.LaunchCode == 3) {
+    			if (this.activity.getClass() != SettingsView.class) {
+    				startActivity(new Intent(this.activity, SettingsView.class));				
+    			}
+    		}
+		}
+
+		@Override
+		public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+			// Do nothing for now
+		}
+	}
+
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tag_view);
-		
-       final Button mainViewButton2 = (Button) findViewById(R.id.buttonMain2);		
-        mainViewButton2.setOnClickListener(
-                new View.OnClickListener()
-                {
-                        @Override
-                        public void onClick(View v)
-                        {
-                            Intent intent = new Intent(v.getContext(), MainActivity.class);
-                            startActivityForResult(intent, 0);
-                        }
-                });
+        ActionBar actionBar = getActionBar();
+        actionBar.setTitle("Article Tags");
+        actionBar.setSubtitle("PHENND Update");
+        
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        final Button favoritesViewButton2 = (Button) findViewById(R.id.buttonFavorites2);
-        favoritesViewButton2.setOnClickListener(
-                new View.OnClickListener()
-                {
-                        @Override
-                        public void onClick(View v)
-                        {
-                            Intent intent = new Intent(v.getContext(), FavoritesView.class);
-                            startActivityForResult(intent, 0);
-                        }
-                });
-        
-        final Button settingsViewButton2 = (Button) findViewById(R.id.buttonSettings2);
-        settingsViewButton2.setOnClickListener(
-                new View.OnClickListener()
-                {
-                        @Override
-                        public void onClick(View v)
-                        {
-                            Intent intent = new Intent(v.getContext(), SettingsView.class);
-                            startActivityForResult(intent, 0);
-                        }
-                });
-        
+        Tab tabCategory = actionBar.newTab();
+        tabCategory.setText("Categories").setIcon(R.drawable.ic_launcher).setTabListener(new TabListener(this,0));
+        Tab tabFavorites = actionBar.newTab();
+        tabFavorites.setText("Favorites").setIcon(R.drawable.ic_launcher).setTabListener(new TabListener(this, 1));
+
+        Tab tabTags = actionBar.newTab();
+        tabTags.setText("Tags").setIcon(R.drawable.ic_launcher).setTabListener(new TabListener(this, 2));
+
+        Tab tabSettings = actionBar.newTab();
+        tabSettings.setText("Settings").setIcon(R.drawable.ic_launcher).setTabListener(new TabListener(this, 3));
+
+        actionBar.addTab(tabTags, true);
+        actionBar.addTab(tabCategory, 0, false);
+        actionBar.addTab(tabFavorites, 1,false);
+        actionBar.addTab(tabSettings, 3, false);
+        actionBar.selectTab(tabTags);
+
         flaggedTags = DataManager.getFlaggedTags(); // REPLACE THIS WITH SINGLETON ACCESS TO DATAMANAGER
         ListView listView = (ListView) findViewById(R.id.listView2);
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, flaggedTags);
@@ -82,13 +115,5 @@ public class TagView extends Activity {
         }); 
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_tag_view, menu);
-		return true;
-	}
 
-	
-	
 }
